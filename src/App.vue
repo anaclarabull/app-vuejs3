56f738a0fe78b2c79de97dcc3d92fceb0eb79d7a
Ana Clara Mendes Bull <anaclaramendesbull@gmail.com>
2023-01-25 15:43:11 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template>
+  <TheHeader v-if="showHeader"/>
+  <div v-show="showName">
+    Nome: {{ firstName }} <br>
+    Sobrenome: {{ lastName }}
+  </div>
+
+  <div v-if="acessLevel === 'admin'">Admin</div>
+  <div v-else-if="acessLevel === 'mkt'">Marketing</div>
+  <div v-else>User</div>
+
   <img alt="Vue logo" src="./assets/logo.png">
+
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue';
 
+// objeto que contém os atributos/variáveis do componente
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TheHeader,
+  },
+  data() {
+    // objeto reativo, onde as variáveis retivas estarão
+    return {
+      showHeader: true,
+      firstName: 'Ana',
+      lastName: 'Bull',
+      showName: false,
+      acessLevel: 'user',
+    }
   }
 }
 </script>
