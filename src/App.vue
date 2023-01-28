eb2ba8d2a2191a1d2925faea7394aa1f35d76bf5
Ana Clara Mendes Bull <anaclaramendesbull@gmail.com>
2023-01-27 22:49:53 -0300

    v-on

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,10 @@
 <template>
   <div>
 
-    <div>
-      One-way data binding <br>
-      <!-- É quando somente o sistema consegue alterar informações.  -->
-      Two-way data binding (bidirecional)<br>
-      <!-- Quando o sistema e o usuários conseguem atualizar um valor, um formulário, por exemplo. Utiliza-se o v-model -->
-      v-model -> formulários
-    </div>
-
-    <div>
-      <input v-model="name" type="text">
-      <!-- O v-model pega o que o usuário digitou no input e consegue colocar na variável -->
-      <br>
-      {{ name }}
-    </div>
-
-    <br>
-
-    <div>
-      <label>Sports</label>
-      <br>
-      <select v-model="sports">
-        <option value="">Escolha</option>
-        <option value="futebol">Futebol</option>
-        <option value="skate">Skate</option>
-        <option value="volei">Volei</option>
-      </select>
-      <br>
-      {{ sports }}
-    </div>
-    <br>
-
-    <div>
-      <label>Newsletter</label>
-      <br>
-      <input type="radio" value="Sim" v-model="newsletter"> Sim
-      <input type="radio" value="Não" v-model="newsletter"> Não
-      <br>
-      {{ newsletter }}
-
-    </div>
-    <br>
-
-    <div>
-      <label>Contrato</label>
-      <br>
-      <input type="checkbox" v-model="contrato"> Aceita nosso termos...
-      <br>
-      {{ contrato }}
-
-    </div>
-    <br>
-
-    <div>
-      <label>Cores que você mais gosta</label>
-      <br>
-      <input type="checkbox" value="Azul" v-model="colors"> Azul
-      <input type="checkbox" value="Amarelo" v-model="colors"> Amarelo
-      <br>
-      {{ colors }}
-
-    </div>
+    <button v-on:click="onClick">
+      <!-- Capture quando tiver um evento do elemento, nesse caso o evento é o click -->
+      Enviar
+    </button>
 
   </div>
 </template>
@@ -71,11 +14,12 @@ export default {
   name: 'App',
   data() {
     return {
-      name: 'Ana',
-      sports: '',
-      newsletter: '',
-      contrato: '',
-      colors: [],
+    }
+  },
+
+  methods:{
+    onClick(){
+      console.log('click')
     }
   }
 }
