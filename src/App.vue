b5df7b153451cae996f86e7050f43f0a46fe9607
Ana Clara Mendes Bull <anaclaramendesbull@gmail.com>
2023-01-28 12:31:38 -0300

    computed

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
 
-   
+   {{ user.first_name }} {{ user.last_name }}
+
+  </div> 
+  <br>
+  <div>
+
+   {{ fullName }}
 
   </div>
 </template>
@@ -16,6 +22,12 @@ export default {
         last_name:'Bull'
       }
     }
+  },
+  computed: {
+    fullName(){
+      return `${this.user.first_name} ${this.user.last_name}`
+    }
+    // Computed é usado quando voce só quer computar o valor uma vez, não ocupando tanta memória
   }
 }
 </script>
