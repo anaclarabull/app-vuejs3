a55e4fd5ebd381b220415a340040d40c9052b9c9
Ana Clara Mendes Bull <anaclaramendesbull@gmail.com>
2023-01-27 23:14:54 -0300

    modif. de eventos

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,27 @@
 
     <button v-on:click="onClick">
       <!-- Capture quando tiver um evento do elemento, nesse caso o evento é o click -->
+      <!-- v-on pode ser substituido por @. ex: @click -->
       Enviar
     </button>
+    <br>
+
+    <button v-on:click.once="onClickOnce">
+      <!-- Só vai disparar o evento uma vez por causa do "once". Esses são chamados de Modificadores de Eventos -->
+      Enviar
+    </button>
+
+    <div @mouseover="onMouseOver">
+      Mouse Over
+    </div>
+    <br>
+
+    <form action="https://www.google.com.br/" @submit="onSubmit">
+      <button type="submit">
+        <!-- type="submit" utiizado para enviar informações -->
+      Enviar
+    </button>
+    </form>
 
   </div>
 </template>
@@ -20,6 +39,15 @@ export default {
   methods:{
     onClick(){
       console.log('click')
+    },
+    onClickOnce(){
+      console.log('click')
+    },
+    onMouseOver(){
+      console.log('mouse over')
+    },
+    onSubmit(){
+      console.log('submit')
     }
   }
 }
